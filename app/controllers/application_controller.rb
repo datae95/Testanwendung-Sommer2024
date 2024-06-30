3d1a788411157978f1b15cb5a5a4e17443da5c77
Daniel <mail@datae.de>
2024-06-30 13:24:07 +0200

    add alert, when action not allowed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
   after_action :verify_pundit_authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   before_action :authenticate_user!
 
@@ -12,5 +13,10 @@ class ApplicationController < ActionController::Base
     else
       verify_authorized
     end
+  end
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back_or_to(root_path)
   end
 end
